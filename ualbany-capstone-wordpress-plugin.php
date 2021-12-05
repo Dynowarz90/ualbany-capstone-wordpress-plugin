32adc91c5f8ec00d6b88809a3580f868ec66da26
masunobu <60934434+masunobu@users.noreply.github.com>
2021-12-05 13:05:50 -0500

    Update ualbany-capstone-wordpress-plugin.php

--- ualbany-capstone-wordpress-plugin.php
+++ ualbany-capstone-wordpress-plugin.php
@@ -47,24 +47,37 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/core-functions.php';
 function uacapstone_options_default() {
 
 	return array(
-		'type'     => esc_html__( 'Local Business', 'uacapstone' ),
+		// 'type'     => esc_html__( 'Local Business', 'uacapstone' ),
+		'select_page'     => esc_html__( 'Dafault', 'uacapstone' ),
 		'name'     => esc_html__( 'Name of local business', 'uacapstone' ),
 		'telephone'     => esc_html__( '+1(555)555-5555', 'uacapstone' ),
 		'description'     => esc_html__( 'Local business', 'uacapstone' ),
-		'Website_URL'     => esc_html__( 'https://schema.org', 'uacapstone' ),
-		'PostalAddress'     => esc_html__( '', 'uacapstone' ),
-		'streetAddress'     => esc_html__( '', 'uacapstone' ),
-		'addressRegion'     => esc_html__( '', 'uacapstone' ),
-		'addressLocality'     => esc_html__( '', 'uacapstone' ),
-
-
-		// 'custom_url'     => 'https://wordpress.org/',
-		// 'custom_title'   => esc_html__('Powered by WordPress', 'uacapstone'), // esc_html__を加えて　他言語化のためのlocarization
-		// 'custom_style'   => 'disable',
-		// 'custom_message' => '<p class="custom-message">'. esc_html__('My custom message', 'uacapstone') .'</p>',
-		// 'custom_footer'  => esc_html__('Special message for users', 'uacapstone'),
-		// 'custom_toolbar' => false,
-		// 'custom_scheme'  => 'default',
+		// 'Website_URL'     => esc_html__( 'https://schema.org', 'uacapstone' ),
+		// 'PostalAddress'     => esc_html__( '', 'uacapstone' ),
+		'streetAddress'     => esc_html__( '123 Main St.', 'uacapstone' ),
+		'addressRegion'     => esc_html__( 'NY', 'uacapstone' ),
+		'addressLocality'     => esc_html__( 'Albany', 'uacapstone' ),
 	);
 
+}
+
+	global $pagenow;
+	if ( in_array( $pagenow, array( 'page.php', 'page-new.php', 'post.php', 'post-new.php' ), true ) ) {
+		add_action( 'admin_print_footer_scripts', 'add_post_enctype' );
+	}
+	// add_action( 'admin_menu', array( &$this, 'add' ) );
+	// add_action( 'save_post', array( &$this, 'save' ) );
+	// add_filter( 'bsf_show_on', array( &$this, 'add_for_id' ), 10, 2 );
+	// add_filter( 'bsf_show_on', array( &$this, 'add_for_page_template' ), 10, 2 );
+/**
+ * Add_post_enctype.
+ */
+function add_post_enctype() {
+	echo '123456789123456789123456789123456789123456789
+	<script type="text/javascript">
+	jQuery(document).ready(function(){
+		jQuery("#post").attr("enctype", "multipart/form-data");
+		jQuery("#post").attr("encoding", "multipart/form-data");
+	});
+	</script>';
 }
